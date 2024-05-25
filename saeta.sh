56e64f1eb7b926972b39d55ef4959a64e1997f20
Salomón Dávalos <88594657+AeruonVex@users.noreply.github.com>
2024-05-25 10:32:44 -0400

    Update saeta.sh

--- saeta.sh
+++ saeta.sh
@@ -3,6 +3,8 @@
 # Instalar las dependencias
 npm install @whiskeysockets/baileys
 npm install pino
+npm install fluent-ffmpeg node-fetch node-webpmux wa-sticker-formatter
+npm install 
 
 # Listar los archivos en el directorio actual
 ls
